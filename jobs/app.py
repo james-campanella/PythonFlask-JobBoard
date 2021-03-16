ec4f7089d6a26fdc91550ff6bb19ccc58bfb973e
James F. Campanella <jamessc@siu.edu>
2021-03-16 11:33:58 -0500

    Got 5th test to pass, pesky 3rd parameter of None was missing.

--- jobs/app.py
+++ jobs/app.py
@@ -5,9 +5,9 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g, '_connection')
+    connection = getattr(g, '_connection', None)
     if connection is None:
-        connection, g._connection = sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     return connection
 
 
